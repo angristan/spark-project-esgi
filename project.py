142bdb3d3c73175e1b8e79789d799f914db3b6a1
Stanislas Lange <stanislas.lange@pm.me>
2021-11-18 14:32:51 +0100

    done

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import datetime
 
+from pyspark.ml.feature import StopWordsRemover, Tokenizer
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
@@ -13,7 +14,10 @@ df = spark.read.option("header", True).csv('full.csv')
 
 # 1. Afficher dans la console les 10 projets Github pour lesquels il y a eu le plus de commit.
 
-df.groupBy('repo').count().orderBy('count', ascending=False).show(n=10)
+df.groupBy('repo') \
+    .count() \
+    .orderBy('count', ascending=False) \
+    .show(n=10)
 
 # Output:
 # +--------------------+--------+
@@ -33,7 +37,11 @@ df.groupBy('repo').count().orderBy('count', ascending=False).show(n=10)
 
 # 2. Afficher dans la console le plus gros contributeur (la personne qui a fait le plus de commit) du projet apache/spark.
 
-df.filter(df.repo == 'apache/spark').groupBy('author').count().orderBy('count', ascending=False).first()
+df.filter(df.repo == 'apache/spark') \
+    .groupBy('author') \
+    .count() \
+    .orderBy('count', ascending=False) \
+    .first()
 
 # Output:
 # Row(author='Matei Zaharia <matei@eecs.berkeley.edu>', count=683)
@@ -43,9 +51,53 @@ df.filter(df.repo == 'apache/spark').groupBy('author').count().orderBy('count', 
 # Enable use of 'E' in time format
 spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
 
-df_dates = df.withColumn("timestamp", unix_timestamp(col("date"), "EEE MMM dd HH:mm:ss yyyy Z")).cast("timestamp")
+df_dates = df.withColumn("timestamp", unix_timestamp(col("date"), "EEE MMM dd HH:mm:ss yyyy Z").cast("timestamp"))
 six_months_ago = datetime.datetime.now() - datetime.timedelta(days=180)
-df_dates.filter(df.repo == 'apache/spark').filter(df_dates["timestamp"] >= six_months_ago.strftime('%Y-%m-%d %H:%M:%S')).groupBy('author').count().orderBy('count', ascending=False).show()
+df_dates.filter(df.repo == 'apache/spark') \
+    .filter(df_dates["timestamp"] >= six_months_ago.strftime('%Y-%m-%d %H:%M:%S')) \
+    .groupBy('author') \
+    .count() \
+    .orderBy('count', ascending=False) \
+    .show()
 
 # Output:
 # nothing because the dataset is outdated.
+
+# 4. Afficher dans la console les 10 mots qui reviennent le plus dans les messages de commit sur l’ensemble des projets.
+
+# remove all rows that have null values on any column
+df_no_null = df.na.drop()
+
+# tokenzie the commit message column
+tokenizer = Tokenizer(inputCol="message", outputCol="words_token")
+tokenized = tokenizer.transform(df_no_null).select('words_token')
+
+# remove stop words from the tokenized column
+remover = StopWordsRemover(inputCol='words_token', outputCol='words_clean')
+# remove empty words
+data_clean = remover.transform(tokenized) \
+    .select('words_clean') \
+    .where(size(col("words_clean")) > 0)
+
+result = data_clean.withColumn('word', explode(col('words_clean'))) \
+    .groupBy('word') \
+    .count() \
+    .sort('count', ascending=False)
+
+result.filter(result.word != '').show()
+
+# Output:
+# +---------------+-----+
+# |           word|count|
+# +---------------+-----+
+# |            fix|82251|
+# |            add|73657|
+# |          merge|71816|
+# |         branch|40341|
+# |         remove|38339|
+# |            use|32890|
+# |         update|31808|
+# |           test|27310|
+# |remote-tracking|21173|
+# |          tests|19183|
+# +---------------+-----+
